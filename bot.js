dcbb4231366a9e792528d3b77edbb1bf4cc07d75
goldensgames <44624859+goldensgames@users.noreply.github.com>
2018-12-08 21:30:41 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -119,7 +119,7 @@ if(msg === prefix + 'REDEEM'){
 		var found = redeem.includes(item)
 			if(found === true){
 				console.log("Item Found");
-				userData[sender.id + message.guild.id].inventory.splice(i);
+				userData[sender.id + message.guild.id].inventory.splice(i, 1);
 				message.channel.send(message.author + " Congratulations on your redemption of " + item + " private message Imposto to claim your prize")
 				console.log(userData[sender.id + message.guild.id].inventory)
 			} else if (found === false){
