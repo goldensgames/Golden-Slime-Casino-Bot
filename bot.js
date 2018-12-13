cd74a9bd5f8bf3f35c2cb3bd6a74864da273fa73
goldensgames <44624859+goldensgames@users.noreply.github.com>
2018-12-13 17:04:28 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -468,7 +468,8 @@ if(parts[0] === prefix){
 	if(command === prefix + "GIVE" + " " + amount + "." + who && sender.id + message.guild.id === "198866287470837760504453118835032066"){
 		console.log("Giving " + amount + " Tokens");
 		var check = message.content.substring(message.content.indexOf("!"));
-		if(check != undefined){
+		console.log(check);
+		if(check === "!"){
 			let id = message.content.substring(message.content.indexOf("!") + 1, message.content.length - 1);
 		} else {
 			let id = message.content.substring(message.content.indexOf("@") + 1, message.content.length - 1);
