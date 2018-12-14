38f33c6319426a16d056f404174d5c1c8c8b1389
goldensgames <44624859+goldensgames@users.noreply.github.com>
2018-12-13 19:04:06 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -476,7 +476,7 @@ if(parts[0] === prefix){
 		var count = parseInt(amount);
 		console.log(id);
 		console.log(id + message.guild.id);
-		if(check === undefined){
+		if(check === message.content.indexOf("!")){
 			userData[idd + message.guild.id].tokens += count;
 		} else{
 			userData[id + message.guild.id].tokens += count;
